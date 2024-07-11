702297414ee1b811527dbba74d5e1aa3e25f6e9c
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2024-07-11 16:35:26 +0300

    refactor first parser version

--- bin/main.ml
+++ bin/main.ml
@@ -13,7 +13,7 @@ let is_digit c = c >= '0' && c <= '9'
 
 let is_whitespace c = c = ' ' || c = '\t' || c = '\n' || c = '\r'
 
-let is_alpha c = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+let is_alpha c = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_')
 
 let ws text pos =
   let length = find_len text in
@@ -37,6 +37,7 @@ let econst text pos =
   acc_num pos ""
 
 let ident_or_keyword text pos =
+  let pos = ws text pos in
   let length = find_len text in
   let rec acc_id pos acc =
     if pos < length && is_alpha text.[pos] then
@@ -50,7 +51,7 @@ let ident_or_keyword text pos =
   acc_id pos ""
 
 let is_keyword s =
-  s = "while" || s = "do" || s = "done" || s = "if" || s = "then" || s = "else"
+  s = "while" || s = "do" || s = "done" || s = "if" || s = "then" || s = "else" || s = "fi"
 
 let ident text pos =
   match ident_or_keyword text pos with
@@ -100,7 +101,6 @@ and factor text pos =
   let pos = ws text pos in
   (* expr in brackets *)
   if pos < find_len text && text.[pos] = '(' then
-    (Printf.printf "here %d \n" pos;
     match expr text (pos + 1) with
     | `Error -> `Error
     | `Success (e, pos) ->
@@ -108,14 +108,14 @@ and factor text pos =
         if pos < find_len text && text.[pos] = ')' then
           `Success (e, pos + 1)
         else
-          `Error)
+          `Error
   (* single const *)
   else if pos < find_len text && is_digit text.[pos] then
     econst text pos
   (* identificator aka var *)
   else if pos < find_len text && is_alpha text.[pos] then
     ident text pos
-  (* unknown shit *)
+  (* unknown token *)
   else
     `Error
 
@@ -137,6 +137,10 @@ let parse_and_print text =
 
 (* example *)
 let () =
-  let input = "1+3+4+(5" in
+  let input = "1+3+3+a_s" in
   parse_and_print input
 
+  (* outside parser calling inside parsers of assignment, while or if 
+     if any of them is true than call outside parser from current inside position
+     else try another inside parser from starting position
+    *)
