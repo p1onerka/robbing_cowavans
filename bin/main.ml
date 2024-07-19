7f41835f30338777ba475b0de671fba1254e7794
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2024-07-19 21:53:37 +0300

    beautify code pt2

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,35 @@
 open Parser.Readfile 
 open Codegen_riscv64.Codegen
 
+(* DEGUB IN-PROGRAM TEST CALLS, need to be removed later *)
+
+(* full programs tests *)
+(* let () =
+  match program "test/full_program_test1.txt" with
+    |`Error -> failwith ""
+    | `Success (prog,_) -> codegen prog *)
+ (* let () =
+  match program "test/full_program_test2.txt" with
+    |`Error -> failwith ""
+    | `Success (prog,_) -> codegen prog *)
+
+(* comparision tests *)
+(* let () =
+  match program "test/comparison_test1.txt" with
+    |`Error -> failwith ""
+    | `Success (prog,_) -> codegen prog
+(* let () =
+  match program "test/comparison_test2.txt" with
+    |`Error -> failwith ""
+    | `Success (prog,_) -> codegen prog *) *)
+
+(* assignment test *)
+(* let () =
+  match program "test/assignment_test.txt" with
+    |`Error -> failwith ""
+    | `Success (prog,_) -> codegen prog  *)
+
+
 let() =
     if Array.length Sys.argv < 2 then failwith 
       "expects 1 argument, recieved 2";
